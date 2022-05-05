ad0ba80f95615ed5243b0c59dbd9557b84cb18b3
harshit1507 <33980701+harshit1507@users.noreply.github.com>
2022-05-05 20:37:33 +0530

    Update vsnew.ps1

--- vsnew.ps1
+++ vsnew.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest "https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Release&version=VS2022&source=VSLandingPage&includeRecommendd=true&cid=2030:a29b340e-780d-45c3-a397-b9cc343fb17b" -OutFile  C:\build\VisualStudioSetup.exe
+Invoke-WebRequest https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2.1/npp.8.2.1.Installer.exe -OutFile c:\npp.8.2.1.Installer.exe
 
-.\VisualStudioSetup.exe  install --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools" --add Microsoft.VisualStudio.Component.Azure.AuthoringTools --downloadThenInstall --passive
+Start-Process C:\npp.8.2.1.Installer.exe /S -NoNewWindow -Wait -PassThru
